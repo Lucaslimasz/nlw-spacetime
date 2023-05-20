fa41912ae01ad557011a4284fbf60abb4114a486
Lucas Lima <lucaslima@MacBook-Air-de-Lucas.local>
2023-05-19 23:57:10 -0300

    configure oauth github and components

--- web/src/Components/Button.tsx
+++
@@ -1,3 +0,0 @@
-export default function Button() {
-  return <p className="text-base md:text-xs">Botão</p>
-}
---
+++ web/src/Components/EmptyMemories.tsx
@@ -0,0 +1,13 @@
+export function EmptyMemories() {
+  return (
+    <div className="jusity-center flex flex-1 items-center">
+      <p className="w-[360px] text-center leading-relaxed">
+        Você ainda não registrou nenhuma lembrança, comece a{' '}
+        <a className="houver:text-gray-50 underline" href="">
+          criar agora
+        </a>
+        !
+      </p>
+    </div>
+  )
+}
